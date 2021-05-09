c9533a1e0bdf088c1b47a09195d179169c852552
R Narayan <53508229+bitoffabyte@users.noreply.github.com>
2021-05-09 18:18:19 +0530

    :bug:

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const axios = require('axios');
 const dotenv = require('dotenv');
-
+// Yeet
 // const img = require('./a.png');
 dotenv.config();
 client.on('ready', () => {
